f95fd3012ba104ecbdd8f282665a9c0853baf012
mere-human <9664141+mere-human@users.noreply.github.com>
2026-01-21 23:28:00 +0200

    Add a test button

--- android/vrec/app/src/main/java/com/example/simpleapp/MainActivity.java
+++ android/vrec/app/src/main/java/com/example/simpleapp/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.simpleapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private Button testButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Find the views by their IDs
+        textView = findViewById(R.id.textView);
+        testButton = findViewById(R.id.testButton);
+
+        // Set up button click listener
+        testButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Change the text when button is clicked
+                textView.setText(getString(R.string.button_clicked));
+            }
+        });
     }
 }
 
